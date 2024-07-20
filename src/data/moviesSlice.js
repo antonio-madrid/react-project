5f3f9d35027204c5c26df5408c32687fd6848902
Antonio Jimenez <antonio.jimenez@e2ycommerce.com>
2024-07-20 02:43:38 +0200

    feat(wip): add infinite scrolling [TASK-3]

--- src/data/moviesSlice.js
+++ src/data/moviesSlice.js
@@ -1,21 +1,41 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-
-export const fetchMovies = createAsyncThunk('fetch-movies', async (apiUrl) => {
-    const response = await fetch(apiUrl)
-    return response.json()
-})
+import { createSlice } from "@reduxjs/toolkit"
+import {fetchMovies} from "../service/FetchMovieService";
 
 const moviesSlice = createSlice({
     name: 'movies',
     initialState: { 
         movies: [],
+        page: 1,
+        hasNextPage: false,
         fetchStatus: '',
     },
     reducers: {},
     extraReducers: (builder) => {
-        builder.addCase(fetchMovies.fulfilled, (state, action) => {
-            state.movies = action.payload
-            state.fetchStatus = 'success'
+        builder.addCase(
+            fetchMovies.fulfilled, (state, action) => {
+                if (!action.payload.results || action.payload.results.length === 0) {
+                    state.movies = [];
+                    state.page = 1;
+                    state.hasNextPage = false;
+                } else {
+                    const movies = action.payload.results;
+                    const currentPage = action.payload.page
+                    const { total_pages } = action.payload;
+
+                    if (!movies || movies.length === 0) {
+                        state.movies = [];
+                    } else if (movies && movies.length > 0) {
+                        currentPage !== 1 ?
+                            (state.movies = state.movies.concat(movies))
+                            : (state.movies = movies)
+
+                        state.hasNextPage = currentPage < total_pages;
+                        // Setting current page number
+                        state.page = currentPage;
+                    }
+
+                }
+                state.fetchStatus = 'success'
         }).addCase(fetchMovies.pending, (state) => {
             state.fetchStatus = 'loading'
         }).addCase(fetchMovies.rejected, (state) => {
