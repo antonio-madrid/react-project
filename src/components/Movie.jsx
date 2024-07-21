c5b5770c208b20cdd88dd3abc0e160d43cc0e978
Antonio Jimenez <antonio.jimenez@e2ycommerce.com>
2024-07-21 13:30:23 +0200

    refactor: reorganize hooks & comments

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,7 +4,6 @@ import watchLaterSlice from '../data/watchLaterSlice'
 import placeholder from '../assets/not-found-500X750.jpeg'
 
 const Movie = ({ movie, viewTrailer, closeCard }) => {
-
     const state = useSelector((state) => state)
     const { starred, watchLater } = state
     const { starMovie, unstarMovie } = starredSlice.actions
